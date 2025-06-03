d0c3705d45a0a9d3dd6be350a38ace7fdb8abfae
dodgedlol2 <dodgedlol2@gmail.com>
2025-06-03 10:47:05 +0200

    Update Price6.py

--- kaspa-analytics/pages/Price6.py
+++ kaspa-analytics/pages/Price6.py
@@ -184,7 +184,7 @@ st.markdown("""
         transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1) !important;
         box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2) !important;
         min-height: 36px !important;
-        width: 40px !important;
+        width: 100px !important;
         max-width: 40px !important;
         min-width: 40px !important;
     }
